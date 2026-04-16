f9f13419ff63aa4f4db91c18ac2c0c1e65698cd2
notsure55 <blackender5123@mailfence.com>
2026-04-16 14:43:18 -0700

    src files

---
+++ src/hook.rs
@@ -0,0 +1,63 @@
+use anyhow::{anyhow, Result};
+use min_hook_rs::hook::*;
+use std::ffi::c_void;
+
+use crate::*;
+
+#[derive(Debug, Clone)]
+pub struct Hook {
+    module: &'static str,
+    name: &'static str,
+    detour: *mut c_void,
+    target: Option<*mut c_void>,
+}
+
+impl Hook {
+    pub const fn new(module: &'static str, name: &'static str, detour: *mut c_void) -> Self {
+        Self {
+            module,
+            name,
+            detour,
+            target: None,
+        }
+    }
+    pub fn enable(&mut self) -> Result<()> {
+        let (trampoline, target) = create_hook_api(self.module, self.name, self.detour)?;
+
+        store_function(self.name.to_string(), trampoline);
+
+        enable_hook(target)?;
+        log::info!("Enabled hook {}", self.name);
+
+        self.target = Some(target);
+
+        Ok(())
+    }
+    pub fn disable(&self) -> Result<()> {
+        if let Some(target) = self.target {
+            if !target.is_null() {
+                disable_hook(target)?;
+                log::info!("Disabled Hook {}", self.name);
+            } else {
+                return Err(anyhow!("Failed to disable hook for {}", self.name));
+            }
+        } else {
+            log::warn!("Target for {} is {:?}", self.name, self.target);
+        }
+
+        Ok(())
+    }
+    pub fn remove(&self) -> Result<()> {
+        if let Some(target) = self.target {
+            if !target.is_null() {
+                remove_hook(target)?;
+                log::info!("Removed Hook {}", self.name);
+            } else {
+                return Err(anyhow!("Failed to remove hook for {}", self.name));
+            }
+        } else {
+            log::warn!("Target for {} is {:?}", self.name, self.target);
+        }
+        Ok(())
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+use anyhow::Result;
+use min_hook_rs::hook::*;
+use std::collections::BTreeMap;
+use std::ffi::c_void;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::LazyLock;
+use std::sync::Mutex;
+
+mod hook;
+use hook::Hook;
+
+pub static ORIGINAL_FUNCTIONS: LazyLock<Mutex<BTreeMap<String, usize>>> =
+    LazyLock::new(|| Mutex::new(BTreeMap::new()));
+
+// thread counters
+pub static HOOK_COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+pub struct Hooks {
+    hooks: Vec<Hook>,
+}
+
+impl Hooks {
+    pub fn new(hooks: &[Hook]) -> Result<Self> {
+        initialize()?;
+        Ok(Self {
+            hooks: hooks.to_vec(),
+        })
+    }
+    pub fn enable(&mut self) -> Result<()> {
+        log::info!("Enabling Hooks!");
+
+        for hook in self.hooks.iter_mut() {
+            hook.enable()?;
+        }
+
+        Ok(())
+    }
+    pub fn disable(&self) -> Result<()> {
+        log::info!("Disabling Hooks!");
+        for hook in self.hooks.iter() {
+            hook.disable()?;
+        }
+
+        // waiting for ref counter to be 0 so we can remove hooks
+        // as to not remove hook while thread is inside of it .
+        while HOOK_COUNTER.load(Ordering::Relaxed) != 0 {
+            std::thread::sleep(std::time::Duration::from_millis(10));
+        }
+
+        log::info!("Removing Hooks!");
+        for hook in self.hooks.iter() {
+            hook.remove()?;
+        }
+
+        uninitialize()?;
+
+        Ok(())
+    }
+}
+
+pub fn get_function(function: &str) -> Option<*mut c_void> {
+    let map = &(ORIGINAL_FUNCTIONS.lock().unwrap());
+    if let Some(original) = map.get(function) {
+        return Some(*original as _);
+    } else {
+        None
+    }
+}
+
+pub fn store_function(name: String, function: *mut c_void) {
+    let map = &mut (ORIGINAL_FUNCTIONS.lock().unwrap());
+    map.insert(name, function as usize);
+}
+
+pub fn increment_counter(counter: &AtomicUsize) {
+    let count = counter.load(Ordering::Relaxed);
+    counter.store(count + 1, Ordering::Relaxed);
+}
+
+pub fn decrement_counter(counter: &AtomicUsize) {
+    let count = counter.load(Ordering::Relaxed);
+    counter.store(count - 1, Ordering::Relaxed);
+}
